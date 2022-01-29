04df297856c7a62b0b39ad27af911843d7d5f893
Aaron L <aliebgold@madisoncollege.edu>
2022-01-29 08:24:22 -0600

    My first Script

---
+++ week2/first_Script.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+fname = input ("What is your full name name? : ")
+
+print(fname)
+
+print(f"\nOne thing i learned in the first week of python class is how to assign a variable and ask for the users input. ")
