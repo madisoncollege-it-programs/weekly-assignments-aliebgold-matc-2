97fcfa2169bdd9c2388c0ab9ef5653d579e004b9
Aaron L <aliebgold@madisoncollege.edu>
2022-03-08 11:39:14 -0600

    Working Porttion of Midterm Slicing

---
+++ midterm/midtermSlicing.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+print("\n")
+print("Name: Aaron Liebgold ")
+print("\n")
+
+varList=["happy","birthday","you","think","you","wrong","right.","are","you","this","think","you","can't","you","today","or","live","can","class","taken","run","go","are","summer","weather","interest","python"]
+
+print(varList[24:25])
+print("\n")
+
+print(varList[2:5:])
+print("\n")
+
+print(varList[17:12:-2])
+print("\n")
+
+print(varList[10:13:])
+print("\n")
+
+print(varList[8:5:-1])
+print("\n")
+
+print("Whether you think you can or you think you can't, you are right.")
